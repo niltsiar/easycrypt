7ce255c2ea8e9d481fa1a73cc93d8430efbc037b
Borja Quevedo <borjota@gmail.com>
2019-05-30 13:35:45 +0200

    ♻️ Move declarations to the beginning of the file

--- library/src/main/kotlin/dev/niltsiar/easycrypt/KeyStorage.kt
+++ library/src/main/kotlin/dev/niltsiar/easycrypt/KeyStorage.kt
@@ -5,19 +5,19 @@ package dev.niltsiar.easycrypt
 import java.security.Key
 import java.security.KeyStore
 
-interface KeyStorage {
-    fun keyExists(): Boolean
-    fun generateKey()
-    fun getKey(): Key
-    val encryptionTransformation: String
-}
-
 @JvmSynthetic
 internal const val ANDROID_KEYSTORE: String = "AndroidKeyStore"
 @JvmSynthetic
 internal const val MASTER_KEY_ALIAS: String = "easy_crypt_master_key"
 @JvmSynthetic
 internal const val IV_SEPARATOR: String = "@"
+
+interface KeyStorage {
+    fun keyExists(): Boolean
+    fun generateKey()
+    fun getKey(): Key
+    val encryptionTransformation: String
+}
 
 @JvmSynthetic
 internal fun getKeyStore(): KeyStore {
